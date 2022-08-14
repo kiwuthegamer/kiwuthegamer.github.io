07b149ab493b579674f09e7a6758bb1ad64ca7f5
kiwuthegamer <74356223+kiwuthegamer@users.noreply.github.com>
2022-08-14 23:16:39 +0530

    Finalized Pricing Section

--- script.js
+++ script.js
@@ -9,8 +9,19 @@ function getAge(dateString) {
   return age;
 }
 
+var offers;
 var elems = [];
 var isMobile;
+var discountMultiplier = 0;
+
+function calculatePrice(price){
+  if(discountMultiplier){
+    return("₹<s>"+price+"</s> "+Math.floor(price*discountMultiplier));
+  } else {
+    return("₹"+price);
+  }
+}
+
 window.onload = function() {
   isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   //var isMobile = 1;
@@ -31,52 +42,64 @@ window.onload = function() {
 // Pricing
 var pricing = {
   "cW": {
-    options: ["0 - 50","50 - 100","100 - 150","150 - 300","300 - 500","500 - 1000","1000+"],
-    prices: ["20","30","40","60","100","150","200"],
+    options: ["0 - 200","200 - 400","400 - 600","600 - 800","800 - 1000","1000+"],
+    prices: ["300","350","400","450","500","600"],
     unit: " Lines"
   },
   "cB": {
     options: ["0 - 200","200 - 350","350 - 500","500 - 750","750 - 1000","1000+"],
-    prices: ["20","35","45","60","80","100"],
+    prices: ["70","85","100","120","150","200"],
     unit: " Characters"
   },
   "cE": {
-    price: "80"
+    price: "120"
   },
   "gdP": {
-    price: "40"
+    price: "100"
   },
   "gdI": {
-    price: "20"
+    price: "50"
   },
   "gdT": {
-    price: "35"
+    price: "100"
   },
   "gdE": {
-    price: "25"
+    price: "80"
   },
   "vpV": {
     options: ["0 - 1","1 - 3","3 - 5","5 - 10","10 - 15","15+"],
-    prices: ["20","30","45","60","75","100"],
+    prices: ["70","100","120","140","160","200"],
     unit: " Minutes"
+  },
+  "vpP": {
+    price: "80"
   },
   "mcA": {
     price: "0"
   },
   "mcC": {
     options: ["0 - 100","100 - 300","300 - 500","500 - 700","700 - 1000","1000 - 2000","2000+"],
-    prices: ["15","25","40","50","70","100","150"],
+    prices: ["20","50","100","150","200","250","350"],
     unit: " Characters"
   },
   "mcRD": {
     options: ["0 - 100","100 - 300","300 - 500","500 - 700","700 - 1000","1000+"],
-    prices: ["20","40","70","100","120","130"],
+    prices: ["80","120","150","200","250","400"],
     unit: " KB"
   },
   "mcS": {
     options: ["Make an Original Skin","Customize a Pre-Existing one"],
-    prices: ["50","30"],
+    prices: ["100","60"],
     unit: ""
+  }
+}
+
+offers = {
+  firstMonth: {
+    name: "First Month Discount",
+    discount: "30% off",
+    discountValue: 0.7,
+    endDate: "Sep 30, 2022 00:00:00"
   }
 }
 
@@ -84,6 +107,14 @@ var pricingChoice1 = document.getElementById("pricingChoice1");
 var pricingChoice2 = document.getElementById("pricingChoice2");
 var pricingPrice = document.getElementById("pricingPrice");
 
+var discount = document.getElementById("discount");
+
+for(var i=0;i<Object.keys(offers).length;i++){
+  var offer = offers[Object.keys(offers)[i]];
+  discount.innerHTML = offer.name +" - "+ offer.discount+ "!";
+  discountMultiplier += offer.discountValue;
+}
+  
 if(isMobile) {
   pricingChoice1.style.scale = "250%";
   pricingChoice2.style.scale = "250%";
@@ -98,10 +129,10 @@ pricingChoice1.onchange = function() {
       }
       pricingChoice2.innerHTML = out;
       pricingChoice2.hidden = false;
-      pricingPrice.innerHTML = "Price: ₹"+pricing[this.value].prices[0];
+      pricingPrice.innerHTML = "Price: "+calculatePrice(pricing[this.value].prices[0]);
     } else {
       pricingChoice2.hidden = true;
-      pricingPrice.innerHTML = "Price: ₹"+pricing[this.value].price;
+      pricingPrice.innerHTML = "Price: "+calculatePrice(pricing[this.value].price);
     }
   } else {
     pricingChoice2.hidden = true;
@@ -110,7 +141,25 @@ pricingChoice1.onchange = function() {
 }
 
 pricingChoice2.onchange = function() {
-  pricingPrice.innerHTML = "Price: ₹"+this.value;
+  pricingPrice.innerHTML = "Price: "+calculatePrice(this.value);
 }
+
+var countDownDate = new Date(offers[Object.keys(offers)[0]].endDate.toString()).getTime();
+  
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("discountTimer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("discountTimer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 
 }
