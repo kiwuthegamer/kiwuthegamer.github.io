1f73ae7dbc10c319c5ff582eb26a828ff48d99bb
Kevin <74356223+kiwuthegamer@users.noreply.github.com>
2023-01-27 20:27:44 +0530

    Global Script, Global Style Added
    CSS cleaned up a bit
    Footer also created on all pages automatically

---
+++ globalScript.js
@@ -0,0 +1,7 @@
+// Copywright Footer
+document.write(`<footer>
+<div class="card-text" style="white-space: pre-line;">
+  <br>
+  Copyright &copy; <script>document.write(new Date().getFullYear())</script> Kevin George (kiwuthegamer) All Rights Reserved<br>
+</div>
+</footer>`)
