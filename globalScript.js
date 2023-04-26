2732e0916bf9d862d1a2485b11d8517d212436b7
Kevin <74356223+kiwuthegamer@users.noreply.github.com>
2023-04-26 23:01:41 +0530

    General
    > Favicon Removed
    > Hire me/Portfolio Worked on
    > Back to home button
    > Etc

--- globalScript.js
+++ globalScript.js
@@ -1,7 +1,12 @@
+// Kiwuthegamer Homepage
+if (!document.body.classList.contains("homepage")) {
+  document.body.innerHTML = `<a href="/"><img class="home-link" src="/Assets/Logo/Red.png"></a>` + document.body.innerHTML
+}
+
 // Copywright Footer
-document.write(`<footer>
+document.body.innerHTML += `<footer>
 <div class="card-text" style="white-space: pre-line;">
   <br>
   Copyright &copy; <script>document.write(new Date().getFullYear())</script> Kevin George (kiwuthegamer) All Rights Reserved<br>
 </div>
-</footer>`)
+</footer>`
