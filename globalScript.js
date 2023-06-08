f93c4ed83db26aed95b63657950d3a366bb29175
Kevin <74356223+kiwuthegamer@users.noreply.github.com>
2023-06-08 13:07:47 +0530

    Styling Overhaul + Theme Selector

--- globalScript.js
+++ globalScript.js
@@ -10,3 +10,31 @@ document.body.innerHTML += `<footer>
   Copyright &copy; <script>document.write(new Date().getFullYear())</script> Kevin George (kiwuthegamer) All Rights Reserved<br>
 </div>
 </footer>`
+
+// Themes
+const themeCount = 2
+
+str = "<div class='theme-picker'>"
+for(var i=0;i<themeCount;i++){
+  str += "<div onclick='setTheme("+i+")'></div>"
+}
+
+document.body.innerHTML = str + "</div>" + document.body.innerHTML
+
+function refreshThemes(){
+  var theme = localStorage.getItem("themeID")
+  if (theme == null){
+    localStorage.setItem("themeID", 0)
+  }
+  for(var i=0;i<themeCount;i++){
+    document.body.classList.remove("theme"+i)
+  }
+  document.body.classList.add("theme"+theme)
+}
+
+function setTheme(id){
+  localStorage.setItem("themeID", id)
+  refreshThemes()
+}
+
+refreshThemes()
